f07ece9a825f6c0e8e3dffca7f26036d950f3ea4
Vick Kumar <Vick.Kumar44@gmail.com>
2023-10-03 18:46:37 -0400

    added disable

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ from langchain_functions import (
 )
 
 
+def disable():
+    st.session_state["disabled"] = True
+
+
 def handle_userinput(user_question):
     response = st.session_state.conversation({"question": user_question})
     st.session_state.chat_history = response["chat_history"]
@@ -26,11 +30,15 @@ def main():
         st.session_state.conversation = None
     if "chat_history" not in st.session_state:
         st.session_state.chat_history = None
+    if "disabled" not in st.session_state:
+        st.session_state["disabled"] = False
 
     st.set_page_config(page_title="Chat with Documents", page_icon=":books:")
     st.header("Chat with your Documents")
 
-    user_question = st.chat_input("Ask a question about your documents")
+    user_question = st.chat_input(
+        "Ask a question about your documents", disabled=st.session_state.disabled
+    )
     if user_question:
         handle_userinput(user_question)
 
@@ -39,7 +47,7 @@ def main():
         files = st.file_uploader(
             "upload files here", accept_multiple_files=True, type=["pdf"]
         )
-        if st.button("Upload"):
+        if st.button("Upload", on_click=disable):
             with st.spinner("uploading"):
                 # get pdf text
                 raw_text = get_pdf_text(files)
