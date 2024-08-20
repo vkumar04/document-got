40c6c6c45c9a485ece23692d9c821458226f7198
Vick Kumar <vick@stiegleredtech.org>
2024-08-20 10:06:46 -0400

    updated imports

--- langchain_functions.py
+++ langchain_functions.py
@@ -1,10 +1,10 @@
 from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
-from langchain.embeddings import HuggingFaceInstructEmbeddings, OpenAIEmbeddings
-from langchain.vectorstores import FAISS
+from langchain_community.embeddings import OpenAIEmbeddings
+from langchain_community.vectorstores import FAISS
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
-from langchain.chat_models import ChatOpenAI
+from langchain_community.chat_models import ChatOpenAI
 from dotenv import load_dotenv
 
 # load env vars
